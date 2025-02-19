88a39f62c355e43a817d62f3fbda550d021183f0
dangdo26 <dangdo.261100@gmail.com>
2025-02-19 22:31:48 +0700

    Initial commit

---
+++ src/main/java/Bai_1_2/Bai1.java
@@ -0,0 +1,9 @@
+package Bai_1_2;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/Bai_1_2/Bai2.java
@@ -0,0 +1,14 @@
+package Bai_1_2;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int resultA = -5 + 8 * 6;
+        int resultB = (55 + 9) % 9;
+        int resultC = 20 + -3 * 5 / 8;
+        int resultD = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(resultA);
+        System.out.println(resultB);
+        System.out.println(resultC);
+        System.out.println(resultD);
+    }
+}
